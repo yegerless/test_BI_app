d777b9112e44099b4f8497f9001bc34ef512e6d3
yegerless <Sergey.sys@mail.ru>
2024-04-16 15:45:48 +0300

    add code for createing ssh-tunnel into collect_data_from_db

--- app/Dashboard/Collect_data_from_db.py
+++ app/Dashboard/Collect_data_from_db.py
@@ -1,17 +1,29 @@
 import psycopg2
+from sshtunnel import SSHTunnelForwarder
 import pandas as pd
 
-conn = psycopg2.connect(host='',
-                        port='', 
-                        dbname='',  
-                        user='', 
-                        password='')
+
+server = SSHTunnelForwarder(('185.91.53.192', 5000), 
+                            ssh_username='root',
+                            ssh_password='BoNGbIakv8y0', 
+                            remote_bind_address=('127.0.0.1', 5432),
+                            # local_bind_address=('127.0.0.1', 5432)
+                            )
+server.start()
+
+
+
+conn = psycopg2.connect(dbname='regions_django',  
+                        user='regions_django', 
+                        host=server.local_bind_host,
+                        port=server.local_bind_port,
+                        password='YjdsqGfhjkmairlow74')
 
 conn.set_session(readonly=True)
 
 cur = conn.cursor()
 
-cur.execute("SELECT * FROM table_name")
+cur.execute("SELECT * FROM monitoring_acs")
 
 data = cur.fetchall()
 
